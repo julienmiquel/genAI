921d6d286c667756e1425038e2275e37b188d125
julien Miquel <julienmiquel@google.com>
2023-12-12 08:53:12 +0000

    Split my experiment in two part
    backend : only rag with vertexai search
    backend multiple strategy for experimenting

--- chatbot/backend-multiple-strategy/app/server.py
+++ chatbot/backend-multiple-strategy/app/server.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 
+#from rag_google_cloud_sensitive_data_protection.chain import chain as rag_google_cloud_sensitive_data_protection_chain
 from rag_google_cloud_vertexai_search.chain import chain as rag_google_cloud_vertexai_search_chain
+from simplechat.chain import chain as simplechat_chain
+from simplechat.agentWikipedia import agentWikipedia as agent_Wikipedia
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def redirect_root_to_docs():
@@ -13,8 +18,15 @@ async def redirect_root_to_docs():
 @app.get("/ping")
 async def ping():
     return {"message": "pong"}
+
     
 add_routes(app, rag_google_cloud_vertexai_search_chain, path="/chat")
+add_routes(app, simplechat_chain, path="/simplechat")
+add_routes(app, agent_Wikipedia, path="/wikipedia")
+
+
+#add_routes(app, rag_google_cloud_sensitive_data_protection_chain, path="/rag-google-cloud-sensitive-data-protection")
+
 
 if __name__ == "__main__":
     import uvicorn
---
+++ chatbot/backend/packages/.ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,1 @@
+# rag chain with vertexai search
